a678dd4c3ac6ef1859ee65d28f7d70c672764816
Tramcnb1804 <tramcnbse184306@fpt.edu.vn>
2025-02-22 19:06:03 +0700

    hhhhh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaSearch, FaShoppingCart } from 'react-icons/fa';
 import './Navbar.css';
 
 function Navbar() {
+    const location = useLocation();
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container flex-column">
@@ -43,16 +45,36 @@ function Navbar() {
                 <div className="w-100 navigation-row">
                     <ul className="nav justify-content-center">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
+                            <Link
+                                className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                                to="/"
+                            >
+                                Home
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/products">Products</Link>
+                            <Link
+                                className={`nav-link ${location.pathname === '/products' ? 'active' : ''}`}
+                                to="/products"
+                            >
+                                Products
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/skin-test">Skin Test</Link>
+                            <Link
+                                className={`nav-link ${location.pathname === '/skin-test' ? 'active' : ''}`}
+                                to="/skin-test"
+                            >
+                                Skin Test
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/blog">Blog</Link>
+                            <Link
+                                className={`nav-link ${location.pathname === '/blog' ? 'active' : ''}`}
+                                to="/blog"
+                            >
+                                Blog
+                            </Link>
                         </li>
                     </ul>
                 </div>
