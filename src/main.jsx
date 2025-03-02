98528b5076f8c484b4c84fed476d5c3671cb633d
Tramcnb1804 <tramcnbse184306@fpt.edu.vn>
2025-03-02 22:20:56 +0700

    uuu

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store'; // Updated import path
 
-import '/src/styles/index.css'
+import '/src/styles/index.css';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addCart } from '../api/cartApi';
+import { addToCart } from '../api/cartApi';
+
 
 const initialState = {
     items: [],
